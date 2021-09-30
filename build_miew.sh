0c5df3c935de4d37fb7a363de7b4d8ac0e815d65
S1ckick <maksim040801@gmail.com>
2021-09-30 17:59:03 +0300

    change path

--- build_miew.sh
+++ build_miew.sh
@@ -3,5 +3,5 @@ cd miew
 yarn
 cd packages/lib && yarn run ci
 cd ../../../
-cp miew/packages/lib/build/dist/Miew.js $PWD/meow/static/
-cp miew/packages/lib/build/dist/Miew.min.css $PWD/meow/static/
+cp miew/packages/lib/build/dist/Miew.js meow/static/
+cp miew/packages/lib/build/dist/Miew.min.css meow/static/
