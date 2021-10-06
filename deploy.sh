ac8dd2f2df26c195581890c8c03304c6a46ca9d9
S1ckick <maksim040801@gmail.com>
2021-10-06 11:20:43 +0300

    fixed deploy bugs

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ wget https://github.com/Kitware/CMake/releases/download/v3.21.3/cmake-3.21.3.tar
 tar -xvzf cmake-3.21.3.tar.gz
 rm -r cmake-3.21.3.tar.gz
 cd cmake-3.21.3
+./bootstrap --prefix=$PWD/../env/
+make
+make install
 cd ..
 rm -r cmake-3.21.3
 wget https://jztkft.dl.sourceforge.net/project/swig/swig/swig-4.0.2/swig-4.0.2.tar.gz
@@ -21,11 +24,11 @@ tar -xvzf openbabel-3-1-1.tar.gz
 rm -r openbabel-3-1-1.tar.gz
 mkdir ob-build
 cd ob-build
-./$PWD/../env/bin/cmake ../openbabel-openbabel-3-1-1 -DPYTHON_BINDINGS=ON -DRUN_SWIG=ON -DCMAKE_INSTALL_PREFIX=$PWD/../env/
+cmake ../openbabel-openbabel-3-1-1 -DPYTHON_BINDINGS=ON -DRUN_SWIG=ON -DCMAKE_INSTALL_PREFIX=$PWD/../env/
 make -j$(nproc --all) install
 cd ../
-cd rm -r ob-build
-cd rm -r openbabel-openbabel-3-1-1
+rm -r ob-build
+rm -r openbabel-openbabel-3-1-1
 pip install plip --no-deps
 pip install numpy
 pip install flask
